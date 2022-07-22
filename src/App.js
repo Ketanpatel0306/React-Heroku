2512a93e8fd6935e7ddbfdc7f073cde4dd228fbb
Ketanpatel0306 <Ketan_patel@codenticsoftware.com>
2022-07-22 15:46:15 +0530

    data item

--- src/App.js
+++ src/App.js
@@ -1,87 +1,17 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import { Config } from "./config";
+import React from "react";
+import SingleItem from "./single-item";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { FindItem } from "./find-item";
 
 function App() {
-  const [save, setSave] = useState([]);
-  const DataFetch = async () => {
-    const response = await fetch(`${Config.BaseUrl}/users`, {
-      method: "GET",
-      headers: { Accept: "application/json" },
-    });
-    const data = await response.json();
-    setSave(data.data);
-  };
-  useEffect(() => {
-    DataFetch();
-  }, []);
   return (
-    <div className="App">
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "space-around",
-          backgroundColor: "yellow",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        {save.map((item, index) => {
-          return (
-            <div
-              key={index + "mapKey"}
-              style={{
-                backgroundColor: index % 2 === 0 ? "pink" : "greenyellow",
-                border: "1px solid black",
-                padding: "0px 30px",
-              }}
-            >
-              <p>
-                <b> Address: </b>
-                {item.address}
-              </p>
-              <p>
-                <b>City:</b> {item.city}
-              </p>
-              <p>
-                <b>CreatedAt:</b> {item.createdAt}
-              </p>
-              <p>
-                <b>Email:</b> {item.email}
-              </p>
-              <p>
-                <b>First Name:</b> {item.first_name}
-              </p>
-              <p>
-                <b> Last Name:</b> {item.last_name}
-              </p>
-              <p>
-                <b> Password:</b> {item.password}
-              </p>
-              <p>
-                <b> Phone:</b> {item.phone}
-              </p>
-              <p>
-                <b> State:</b> {item.state}
-              </p>
-              <p>
-                <b>Status:</b> {item.status ? "true" : "false"}
-              </p>
-              <p>
-                <b>UId:</b> {item.u_id}
-              </p>
-              <p>
-                <b>UpdatedAt:</b> {item.updatedAt}
-              </p>
-              <p>
-                <b> Zip:</b> {item.zip}
-              </p>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SingleItem />}></Route>
+        <Route path="/find-item/:id" element={<FindItem />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/single-item.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { Config } from "./config";
+
+const SingleItem = () => {
+  const [save, setSave] = useState([]);
+
+  const DataFetch = async () => {
+    const response = await fetch(`${Config.BaseUrl}/users`, {
+      method: "GET",
+      headers: { Accept: "application/json" },
+    });
+    const data = await response.json();
+    setSave(data.data);
+  };
+
+  useEffect(() => {
+    DataFetch();
+  }, []);
+  return (
+    <div>
+      <div className="App">
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+            backgroundColor: "yellow",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          {save.map((item, index) => {
+            return (
+              <NavLink to={`/find-item/${item.id}`}>
+                <div
+                  key={index + "mapKey"}
+                  style={{
+                    backgroundColor: index % 2 === 0 ? "pink" : "greenyellow",
+                    border: "1px solid black",
+                    padding: "0px 30px",
+                  }}
+                >
+                  <p>
+                    <b> Address: </b>
+                    {item.address}
+                  </p>
+                  <p>
+                    <b>City:</b> {item.city}
+                  </p>
+                  <p>
+                    <b>CreatedAt:</b> {item.createdAt}
+                  </p>
+                  <p>
+                    <b>Email:</b> {item.email}
+                  </p>
+                  <p>
+                    <b>First Name:</b> {item.first_name}
+                  </p>
+                  <p>
+                    <b> Last Name:</b> {item.last_name}
+                  </p>
+                  <p>
+                    <b> Password:</b> {item.password}
+                  </p>
+                  <p>
+                    <b> Phone:</b> {item.phone}
+                  </p>
+                  <p>
+                    <b> State:</b> {item.state}
+                  </p>
+                  <p>
+                    <b>Status:</b> {item.status ? "true" : "false"}
+                  </p>
+                  <p>
+                    <b>UId:</b> {item.u_id}
+                  </p>
+                  <p>
+                    <b>UpdatedAt:</b> {item.updatedAt}
+                  </p>
+                  <p>
+                    <b> Zip:</b> {item.zip}
+                  </p>
+                </div>
+              </NavLink>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleItem;
